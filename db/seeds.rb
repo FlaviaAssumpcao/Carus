2253e39b9349e82c1fcc0e0ef7459b18daa5a388
Joana Cruz <joana.if.cruz@gmail.com>
2022-08-30 19:17:30 +0100

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 puts "Cleaning database..."
-Nonprofit.destroy_all
-User.destroy_all
-TimeCategory.destroy_all
-GoodsCategory.destroy_all
 GoodsCategorization.destroy_all
 TimeCategorization.destroy_all
+TimeCategory.destroy_all
+GoodsCategory.destroy_all
+Nonprofit.destroy_all
+User.destroy_all
 
 
 
@@ -39,136 +39,130 @@ end
 
 
 puts "Creating nonprofits..."
-ventos_ativos = {
-  name: "Associacao Ventos Ativos",
+ventos_ativos = Nonprofit.new(
+  name: "Associação Ventos Ativos",
   brief_description: "A nonprofit organization aiming the protection of the youth through sports, dance and art",
   address: "Alto do Varejao, Lisboa",
   phone: "212 324 543",
+  email: "info@ventosativos.pt",
   city: "Lisboa",
-  full_description: "Through sports, dance and art the Associacao Portuguesa Pro-Juventude provide activities, workshop and regular training through the selection of commited young kids",
+  full_description: "Through sports, dance and art the Associação Ventos Ativos provides activities, workshop and regular training through the selection of commited young kids",
   min_time: 10,
   max_time: 12
-}
+)
+ventos_ativos.save!
+puts "Created #{ventos_ativos.name}"
 
-barriga_cheia = {
-  name: "Associacao Barriga Cheia",
+barriga_cheia = Nonprofit.new(
+  name: "Associação Barriga Cheia",
   brief_description: "A nonprofit organization collecting and distributing food for the people in need",
   address: "Rua do Conde de Redondo 91B, Lisboa",
   phone: "212 100 200",
+  email: "info@barrigacheia.pt",
   city: "Lisboa",
-  full_description: "The Associacao Barriga Cheia collects, organizes and distributes food on different locations on a weekly basis for the people in need. ",
+  full_description: "The Associação Barriga Cheia collects, organizes and distributes food on different locations on a weekly basis for the people in need. ",
   min_time: 14,
   max_time: 17
-}
+)
+barriga_cheia.save!
+puts "Created #{barriga_cheia.name}"
 
-pro_juventude  = {
+pro_juventude  = Nonprofit.new(
   name: "Grupo Pro-juventude",
   brief_description: "A nonprofit organization - entertainment",
   address: "Alto do Varejao, Lisboa",
-  phone: "212 324 543",
+  phone: "212 324 000",
+  email: "info@projuventude.pt",
   city: "Lisboa",
-  full_description: "Through sports, dance and art the Associacao Portuguesa Pro-Juventude provide activities, workshop and regular training through the selection of commited young kids"
-}
+  full_description: "Grupo Pro-Juventude provides activities, workshop and regular training for teenagers from disadvantaged backgrounds"
+)
+pro_juventude.save!
+puts "Created #{pro_juventude.name}"
 
-longevidade = {
-  name: "Associacao de Apoio a Longevidade",
+longevidade = Nonprofit.new(
+  name: "Associação de Apoio à Longevidade",
   brief_description: "A nonprofit organization looking after the older adults",
   address: "Avenida Madame Curie, 18, Lisboa",
   phone: "216 000 222",
+  email: "info@longevidade.pt",
   city: "Lisboa",
-  full_description: "The Associacao de Apoio a Longevidade invest all its effort on the wellbeing of its guests. It provides basic care and entertainment to the older adults",
+  full_description: "Associação de Apoio à Longevidade invests all its effort on the wellbeing of its guests. It provides basic care and entertainment to the older adults",
   min_time: 10,
   max_time: 14
-}
+)
+longevidade.save!
+puts "Created #{longevidade.name}"
 
-
-[ventos_ativos, barriga_cheia, pro_juventude, longevidade].each do |attributes|
-  nonprofit = Nonprofit.create!(attributes)
   # file = URI.open(attributes[:url])
   # experience.photo.attach(io: file, filename: "experience.jpg", content_type: "image/jpg")
   # experience.save
 
-  puts "Created #{nonprofit.name}"
-end
-
 
 puts "Creating goods categories..."
-goods_category_food = {
+goods_category_food = GoodsCategory.create!(
   name: "Food"
-}
+)
 
-goods_category_entertainment = {
+goods_category_entertainment = GoodsCategory.create!(
   name: "Entertainment"
-}
+)
 
-goods_category_household = {
+goods_category_household = GoodsCategory.create!(
   name: "Household"
-}
+)
 
-goods_category_sports = {
+goods_category_sports = GoodsCategory.create!(
   name: "Sports"
-}
+)
 
-goods_category_clothing = {
+goods_category_clothing = GoodsCategory.create!(
   name: "Clothing"
-}
+)
 
-goods_category_tecnology = {
+goods_category_tecnology = GoodsCategory.create!(
   name: "Tecnology"
-}
+)
 
-goods_category_babieskids = {
+goods_category_babieskids = GoodsCategory.create!(
   name: "Babies & Kids"
-}
-
-[goods_category_food, goods_category_entertainment, goods_category_household, goods_category_sports, goods_category_clothing, goods_category_tecnology, goods_category_babieskids].each do |attributes|
-  goods_category = GoodsCategory.create!(attributes)
-  puts "Created #{goods_category.name}"
-end
-
+)
 
 
 puts "Creating time categories..."
-time_category_chidren = {
+time_category_chidren = TimeCategory.create!(
   name: "Children & Youth"
-}
+)
 
-time_category_elderly = {
+time_category_elderly = TimeCategory.create!(
   name: "Elderly"
-}
+)
 
-time_category_animals = {
+time_category_animals = TimeCategory.create!(
   name: "Animals"
-}
+)
 
-time_category_disability = {
+time_category_disability = TimeCategory.create!(
   name: "People with disability"
-}
+)
 
-time_category_unsheltered = {
+time_category_unsheltered = TimeCategory.create!(
   name: "Unsheltered"
-}
+)
 
-time_category_environment = {
+time_category_environment = TimeCategory.create!(
   name: "Environment"
-}
-
-[time_category_chidren, time_category_elderly, time_category_animals, time_category_disability, time_category_unsheltered, time_category_environment].each do |attributes|
-  time_category = TimeCategory.create!(attributes)
-  puts "Created #{time_category.name}"
-end
+)
 
 
 puts "Creating goods categorizations..."
-
-GoodsCategorization.create(nonprofit_id: ventos_ativos.id, goods_category_id: goods_category_entertainment)
-GoodsCategorization.create(nonprofit_id: pro_juventude.id, goods_category_id: goods_category_sports)
+GoodsCategorization.create!(nonprofit: ventos_ativos, goods_category: goods_category_entertainment)
+GoodsCategorization.create!(nonprofit: pro_juventude, goods_category: goods_category_sports)
 
 
 puts "Creating time categorizations..."
 
-TimeCategorization.create(nonprofit_id: longevidade.id, time_category_id: time_category_elderly)
-TimeCategorization.create(nonprofit_id: barriga_cheia.id, time_category_id: time_category_unsheltered)
+TimeCategorization.create!(nonprofit: longevidade, time_category: time_category_elderly)
+TimeCategorization.create!(nonprofit: barriga_cheia, time_category: time_category_unsheltered)
 
 
 puts "Finished!"
