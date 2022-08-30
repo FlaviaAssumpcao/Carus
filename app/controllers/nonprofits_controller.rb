d1f23f00e6a17c8839985c5e7c2e21cb059692c5
Joana Cruz <joana.if.cruz@gmail.com>
2022-08-30 16:25:05 +0100

    nonprofits controller setup for goods and volunteer filter pages

--- app/controllers/nonprofits_controller.rb
+++ app/controllers/nonprofits_controller.rb
@@ -1,6 +1,20 @@
 class NonprofitsController < ApplicationController
 
   def index
+  end
+
+  def goods
+    @nonprofits = Nonprofit.all
+    @nonprofits = @nonprofits.where(goods_category_id: params[:goods_category_id]) if params[:goods_category_id]
+    @nonprofits = @nonprofits.where(city: params[:city]) if params[:city]
+    @goods_categories = GoodsCategory.all
+  end
+
+  def volunteer
+    @nonprofits = Nonprofit.all
+    @nonprofits = @nonprofits.where(goods_category_id: params[:time_category_id]) if params[:time_category_id]
+    @nonprofits = @nonprofits.where(city: params[:city]) if params[:city]
+    @time_categories = TimeCategory.all
 
   end
 
