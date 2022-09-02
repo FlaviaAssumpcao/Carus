92b55fe5a88afcfe805d6beebf155fe99d16b70d
Joana Cruz <joana.if.cruz@gmail.com>
2022-09-02 16:21:15 +0100

    fixed map error

--- app/models/nonprofit.rb
+++ app/models/nonprofit.rb
@@ -12,6 +12,6 @@ class Nonprofit < ApplicationRecord
 
   validates :min_time, comparison: { less_than: :max_time }
 
-  geocoded_by :address
+  #geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
